a28e2da1037f90edd18af0fa9933ff5904680e28
Graham Barber <grahammbarber@gmail.com>
2018-09-07 15:13:09 -0700

    feat(theme): add fonts and custom colors

--- theme.js
+++ theme.js
@@ -1,12 +1,18 @@
-import theme from 'mdx-deck/themes'
+import { swiss as theme } from 'mdx-deck/themes'
 
 export default {
   ...theme,
+  colors: {
+    link: '#D73F09',
+    text: 'black',
+    background: 'white'
+  },
 
+  font: 'Lato, sans-serif',
+  monospace: 'Fira Mono, monospace'
   // Customize your presentation theme here.
   //
   // Read the docs for more info:
   // https://github.com/jxnblk/mdx-deck/blob/master/docs/theming.md
   // https://github.com/jxnblk/mdx-deck/blob/master/docs/themes.md
-
 }
